8672e10e4f2df41e2121e5ee6e220253523d807c
OHMU <proton.muriel@outlook.fr>
2023-09-06 13:09:34 +0200

    Adding a setup for this test

--- src/test/java/com/example/MyRecordRepositoryTest.java
+++ src/test/java/com/example/MyRecordRepositoryTest.java
@@ -4,7 +4,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.Date;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
@@ -18,22 +20,40 @@ import com.example.model.enums.ThemeGeneral;
 import com.example.model.enums.TypeOfTransaction;
 import com.example.repository.MyRecordRepository;
 import com.example.resources.MyRecordDataFactory;
+
+import io.restassured.RestAssured;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Root;
+
 import com.example.MyRecordHTMLCrudTest;
  
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = Replace.NONE)
 @Rollback(false)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class MyRecordRepositoryTest {
+
+    private static MyRecord myRecordForTest;
+
     @Autowired
     private TestEntityManager entityManager;
      
     @Autowired
     private MyRecordRepository repo;
 
-    @Test
-    public void testCreateUser() {
+    @BeforeAll
+    public void setup(){
+        // clear persistence with entityManager // Maybe not necessary 
+        repo.deleteAll();
+        // Create a dummy record
+        myRecordForTest = repo.save(MyRecordDataFactory.createOneMyRecordForTesting());
+    }
 
-        MyRecord myRecordForTest = repo.save(MyRecordDataFactory.createOneMyRecordForTesting());
+    @Test
+    public void testCreateMyRecord() {
         
         MyRecord existsMyRecord = entityManager.find(MyRecord.class, myRecordForTest.getId());
         
