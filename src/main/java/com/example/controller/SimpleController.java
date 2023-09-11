1588bb6b30014f9baf7501d294e969b5594ab197
OHMU <proton.muriel@outlook.fr>
2023-09-11 15:44:45 +0200

    Adding date to HTML form

--- src/main/java/com/example/controller/SimpleController.java
+++ src/main/java/com/example/controller/SimpleController.java
@@ -1,8 +1,10 @@
 package com.example.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.ui.Model;
 
 import com.example.model.entities.MyRecord;
@@ -26,7 +28,13 @@ public class SimpleController {
         return "/account-form";
     }
 
-
+    @PostMapping("/process-account-form")
+    public String processAccountForm(MyRecord myRecord) {
+        
+        myRecordRepository.save(myRecord);
+        
+        return "success";
+    }
 
 
 }
