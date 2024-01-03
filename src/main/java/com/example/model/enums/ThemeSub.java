23e6d5cc95bd4223c1a35a05240d4c965c84b14f
OHMU <proton.muriel@outlook.fr>
2024-01-03 19:30:42 +0100

    Adding conditions to the display of ThemeSub in the form.

--- src/main/java/com/example/model/enums/ThemeSub.java
+++ src/main/java/com/example/model/enums/ThemeSub.java
@@ -1,29 +1,37 @@
 package com.example.model.enums;
 
+import static com.example.model.enums.ThemeGeneral.*;
+
+import java.util.Arrays;
+import java.util.List;
+
 import lombok.Getter;
 
+
 public enum ThemeSub {
-    Null(""),
-    RENT("Loyer"), 
-    CONSTRUCTION_WORK("Travaux"), 
-    COSTS("Coût"), 
-    PROPERTY_TAXES("Taxe foncière"), 
-    WATER("Eau"), 
-    GASS("Gaz"), 
-    ELECTRICITY("Electircité"), 
-    LOAN("Prêt"), 
-    FIRE_WOOD("Bois de chauffe"), 
-    INSURANCE("Assurance"), 
-    BOILER("Chaudière"), 
-    CHIMNEY_SWEEPING("Ramonage"), 
-    HOME_APPLIANCE("Electroménager"), 
-    FURNITURE("Mobilier"), 
-    PATRICK("Patrick"), 
-    MURIEL("Muriel");
+    NULL("", Arrays.asList(ThemeGeneral.values())),
+    RENT("Loyer", Arrays.asList(CALUIRE_ET_CUIRE)),
+    CONSTRUCTION_WORK("Travaux",Arrays.asList(CALUIRE_ET_CUIRE, MEZIEU)),
+    COSTS("Coût",Arrays.asList(CALUIRE_ET_CUIRE, MEZIEU)),
+    PROPERTY_TAXES("Taxe foncière",Arrays.asList(CALUIRE_ET_CUIRE, MEZIEU)),
+    WATER("Eau",Arrays.asList(CALUIRE_ET_CUIRE, MEZIEU)),
+    GASS("Gaz",Arrays.asList(CALUIRE_ET_CUIRE, MEZIEU)),
+    ELECTRICITY("Electircité",Arrays.asList(CALUIRE_ET_CUIRE, MEZIEU)),
+    LOAN("Prêt",Arrays.asList(MEZIEU)),
+    FIRE_WOOD("Bois de chauffe",Arrays.asList(MEZIEU)),
+    INSURANCE("Assurance",Arrays.asList(CALUIRE_ET_CUIRE, MEZIEU)),
+    BOILER("Chaudière",Arrays.asList(CALUIRE_ET_CUIRE, MEZIEU)),
+    CHIMNEY_SWEEPING("Ramonage",Arrays.asList(CALUIRE_ET_CUIRE, MEZIEU)),
+    HOME_APPLIANCE("Electroménager",Arrays.asList(CALUIRE_ET_CUIRE, MEZIEU)),
+    FURNITURE("Mobilier",Arrays.asList(CALUIRE_ET_CUIRE, MEZIEU, MONTPLAT)),
+    PATRICK("Patrick",Arrays.asList(CLOTHING, LEISURE,HEALTH,TCL,STATIONARY,INCOME_TAXE)),
+    MURIEL("Muriel",Arrays.asList(CLOTHING, LEISURE, HEALTH,TCL,STATIONARY,INCOME_TAXE));
 
     private @Getter String name;
+    private @Getter List<ThemeGeneral> listGenerals;
 
-    private ThemeSub(String name){
+    private ThemeSub(String name, List<ThemeGeneral> list){
         this.name= name;
+        this.listGenerals = list;
     }
 }
